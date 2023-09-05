36bf87f620406ef4783e8e0c4bbc46985491c676
ooglec <delalie@reallygreattech.com>
2023-09-05 11:30:02 +0100

    new2 changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { generateEIP712Signature, createSalt } from './api/_createSigTest.js'
 import express from 'express'
 import dotenv from 'dotenv'
 import { ethers, utils, Contract} from "ethers";
@@ -10,6 +9,70 @@ const IFactory = new utils.Interface(factoryAbi)
 const factory = '0x762C2b5165E57A9B7B843F5B968C11Fe1d2F55Dd'
 const provider = new ethers.providers.JsonRpcProvider('https://arb1.arbitrum.io/rpc')
 const FactoryContract = new Contract(factory, IFactory, provider)
+export function createSalt(userAddress, nonce) {
+  const salt = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ['address', 'uint256'],
+      [userAddress, nonce]
+    )
+  );
+  return salt;
+}
+
+export async function generateEIP712Signature(wallet, factoryAddress, userAddress, compounderAddress, nonce) {
+
+const domain = {
+  name: 'PlsAutoCompounderFactory',
+  version: '1',
+  chainId: 42161,
+  verifyingContract: factoryAddress,
+};
+
+const types = {
+  EIP712Domain: [
+    { name: 'name', type: 'string' },
+    { name: 'version', type: 'string' },
+    { name: 'chainId', type: 'uint256' },
+    { name: 'verifyingContract', type: 'address' },
+  ],
+  registerCompounder: [  // Changed to match the Solidity type name
+    { name: 'user', type: 'address' },
+    { name: 'compounder', type: 'address' },
+    { name: 'factory', type: 'address' },
+    { name: 'nonce', type: 'uint256' },
+    { name: 'deadline', type: 'uint48' },
+  ],
+};
+
+const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+
+const value = {
+  user: userAddress,
+  compounder: compounderAddress,
+  factory: factoryAddress,
+  nonce: nonce,
+  deadline: deadline,
+};
+
+const signature = await wallet._signTypedData(domain, { registerCompounder: types.registerCompounder }, value);
+const salt = createSalt(userAddress, nonce);
+
+const recoveredAddress = utils.recoverAddress(
+  utils._TypedDataEncoder.hash(domain, { registerCompounder: types.registerCompounder }, value),
+  signature
+);
+
+console.log("Original Address:", wallet.address);
+console.log("Recovered Address:", recoveredAddress);
+
+return {
+  signature: signature,
+  deadline: deadline,
+  nonce: nonce,
+  salt: salt
+};
+
+}
 
 const app = express()
 const PORT = 4000
